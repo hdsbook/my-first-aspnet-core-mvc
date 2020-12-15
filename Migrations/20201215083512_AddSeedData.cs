dd1ddc59710472dd01b5a5119d0de524fbb6ed81
zang <myginnygo@gmail.com>
2020-12-15 16:36:36 +0800

    seeding(add migration and update database)
    dotnet ef migrations add AddSeedData
    dotnet ef database update

---
+++ Migrations/20201215083512_AddSeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcFriends.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Friends",
+                columns: new[] { "Id", "Email", "Mobile", "Name" },
+                values: new object[] { 1, "mary@gmail.com", "0922-355822", "Mary" });
+
+            migrationBuilder.InsertData(
+                table: "Friends",
+                columns: new[] { "Id", "Email", "Mobile", "Name" },
+                values: new object[] { 2, "david@gmail.com", "0933-123456", "David" });
+
+            migrationBuilder.InsertData(
+                table: "Friends",
+                columns: new[] { "Id", "Email", "Mobile", "Name" },
+                values: new object[] { 3, "rose@gmail.com", "0955-888-163", "Rose" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
